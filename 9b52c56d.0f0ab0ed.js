c335848c095853fc2641850dff0dce6b0cb4d46a
gonwi <gonzalo.astorgaie@gmail.com>
2020-01-07 21:05:04 -0800

    Deploy website version based on c232368f7f4f822f4ff8540fe62c08a0ba2a3e81

--- 9b52c56d.0f0ab0ed.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[70],{166:function(n,t,e){"use strict";e.r(t);var r=e(0),a=e.n(r),o=e(258);t.default=function(){return a.a.createElement(o.a,{to:"/releases/latest/download/"})}},258:function(n,t,e){"use strict";var r=e(17);e.d(t,"a",(function(){return r.c})),e.d(t,"b",(function(){return r.d}))}}]);
